1c36496adf494a3413ff6a59fcb1d2bcc0837417
fatemehkondeji <fatemehkondejiworks@gmail.com>
2024-09-12 02:47:31 -0700

    project done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Data from './MOCK_DATA.json';
 
 const App = () => {
+  const[searchTerm,setSearchTerm] =useState("");
   return (
-    <div>
-      
+    <div className='app'>
+      <input type="text" placeholder='search ...'  onChange={(event)=>{setSearchTerm(event.target.value)} }/>
+      {
+        Data.filter ((item)=>{
+          if(searchTerm == ""){
+            return item
+          } else if (item.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
+            return item
+
+          }
+
+        }).map((item,key) => {
+          return(
+           <div className='user' key={key}>
+            <p>{item.first_name}</p>
+            </div>
+          );
+        })
+      }
     </div>
   );
 };
